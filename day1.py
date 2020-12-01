25d9f82bb0ec24a2627f39dd206c5759faba96a2
syncopsta <6129721+syncopsta@users.noreply.github.com>
2020-12-01 14:52:23 +0100

    Update day1.py
    fixing the second part solution

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+start = timer()
 f = open("input_day1", 'r')
 
 nums = f.readlines()
@@ -8,10 +9,17 @@ for n in nums:
     if n+m == 2020:
       print("%s + %s = 2020" % (n, m))
       print("%s * %s = %i" % (n, m, n*m))
-
+      break
+  else:
+    continue
+  break
 for n in nums:
   for m in nums:
     for i in nums:
       if n+m+i == 2020:
         print("%s + %s + %s = 2020" % (n, m, i))
-        print("%s * %s * %s = %i" % (n, m,i, n*m))
+        print("%s * %s * %s = %i" % (n, m,i, n*m*i))
+        break
+    else:
+      continue
+    break
