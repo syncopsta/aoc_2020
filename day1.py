3b70f4c90c1536da74ee99b8e7593faca730bbf2
syncopsta <6129721+syncopsta@users.noreply.github.com>
2020-12-01 14:15:53 +0100

    Update day1.py
    Optimization of the number input

--- day1.py
+++ day1.py
@@ -1,16 +1,17 @@
-numbers = open('input_day1').read().split('\n')
-numbers.pop()
-for n in numbers:
-  for m in numbers:
+f = open("input_day1", 'r')
+
+nums = f.readlines()
+nums = [int(i) for i in nums]
+
+for n in nums:
+  for m in nums:
     if int(n)+int(m) == 2020:
       print("%s + %s = 2020" % (n, m))
       print("%s * %s = %i" % (n, m, int(n)*int(m)))
 
-
-for n in numbers:
-  for m in numbers:
-    for i in numbers:
+for n in nums:
+  for m in nums:
+    for i in nums:
       if int(n)+int(m)+int(i) == 2020:
         print("%s + %s + %s = 2020" % (n, m, i))
         print("%s * %s * %s = %i" % (n, m,i, int(n)*int(m)))
-
