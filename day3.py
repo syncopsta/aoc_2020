6ac2e457769c9d329979c497079194ae819cb3c0
syncopsta <syncopsta@syncopsta.de>
2020-12-03 09:04:35 +0100

    removing timer from day 3

--- day3.py
+++ day3.py
@@ -1,6 +1,3 @@
-from timeit import default_timer as timer
-start = timer()
-
 def tree_hitter(right, down):
   counter = 0
   for i, row in enumerate(grid[::down]):
@@ -16,6 +13,3 @@ counter = 1
 for multiply in [tree_hitter(right, down) for right, down in [(1,1), (3,1), (5,1), (7,1), (1,2)]]:
   counter *= multiply
 print(counter)
-
-end = timer()
-print(end - start)
